49ef0d5716cdab51b5468f76d48488eaa5c9570a
kang.yang <kang.yang@b-and-qchina.com>
2020-10-15 11:52:14 +0800

    vue tabbar

---
+++ app/19-tabbar/tabbar/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <tab-bar>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/home.svg">
+        <img slot="item-icon-active" src="./assets/img/home_active.svg">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/category.svg">
+        <img slot="item-icon-active" src="./assets/img/category_active.svg">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/cart.svg">
+        <img slot="item-icon-active" src="./assets/img/cart_active.svg">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/profile.svg">
+        <img slot="item-icon-active" src="./assets/img/profile_active.svg">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from "./components/tabbar/TabBar";
+import TabBarItem from "./components/tabbar/TabBarItem";
+
+export default {
+  name: 'App',
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+<style>
+/*在style标签中引入css文件*/
+@import "./assets/css/base.css";
+
+</style>
