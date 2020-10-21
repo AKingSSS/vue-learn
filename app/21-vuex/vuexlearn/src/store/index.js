6321a2a952a1ab30c8a777e4d26508766e7ff97b
kang.yang <kang.yang@b-and-qchina.com>
2020-10-21 17:09:25 +0800

    vue vuex基本使用

---
+++ app/21-vuex/vuexlearn/src/store/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// 1.安装插件
+Vue.use(Vuex)
+
+// 2.创建对象
+const store = new Vuex.Store({
+  state:{
+    counter:3
+  },
+  mutations:{
+
+  },
+  actions:{
+
+  },
+  getters:{
+
+  },
+  modules:{
+
+  }
+})
+
+// 3.导出store独享
+export default store
